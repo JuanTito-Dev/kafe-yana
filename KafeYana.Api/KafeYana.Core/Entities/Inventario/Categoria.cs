c402c508f987a4adba5a9375dc6590a2ae5e594d
JuanJ189 <184328882+JuanTito-Dev@users.noreply.github.com>
2026-03-18 22:55:01 -0400

    Primera Subida
    Primera Subida

---
+++ KafeYana.Api/KafeYana.Core/Entities/Inventario/Categoria.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KafeYana.Core.Entities.Inventario
+{
+    public class Categoria
+    {
+        public required string Nombre { get ; set; }
+
+        public string CategoriaPadre { get; set; } = string.Empty;
+
+        public string DescripcionCorta {  get; set; } = string.Empty;
+
+        public required int Orden { get; set; }
+
+        public required bool Menu {  get; set; }
+
+        public string Color {  get; set; } = string.Empty;
+    }
+}
