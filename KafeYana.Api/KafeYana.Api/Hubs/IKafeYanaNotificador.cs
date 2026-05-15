b3427a674bca074d9712de7d251c0e78ca12829a
JuanJ189 <184328882+JuanTito-Dev@users.noreply.github.com>
2026-05-15 09:23:46 -0400

    No ps quien fue

---
+++ KafeYana.Api/KafeYana.Api/Hubs/IKafeYanaNotificador.cs
@@ -0,0 +1,10 @@
+namespace KafeYana.Api.Hubs
+{
+    public interface IKafeYanaNotificador
+    {
+        Task NotificarMesaActualizada(MesaActualizadaPayload payload);
+        Task NotificarNuevaRonda(NuevaRondaPayload payload);
+        Task NotificarVentaProcesada(VentaPayload payload);
+        Task NotificarPedidoParaLlevarActualizado(ParaLlevarPayload payload);
+    }
+}
